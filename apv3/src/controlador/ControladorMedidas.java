051c74dc30a346cf42209a1e59c786429b6a350b
Leo7GG <37249406+Leo7GG@users.noreply.github.com>
2018-03-23 23:14:40 -0600

    Controlador

---
+++ apv3/src/controlador/ControladorMedidas.java
@@ -0,0 +1,39 @@
+package controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ControladorMedidas {
+
+    @FXML
+    private Button btnReporte;
+
+    @FXML
+    private Label lblMensaje;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Button btnNuevo;
+
+    @FXML
+    private CheckBox ckbInactivos;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TextField txtBuscador;
+
+    @FXML
+    private Button btnModificar;
+
+}
+
